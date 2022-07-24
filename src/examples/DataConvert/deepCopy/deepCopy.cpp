534394b1455173351f85291382e4e474c955523a
hudi <1794040692@qq.com>
2022-07-24 23:07:36 +0800

    feat:deepCopyCode

--- src/examples/DataConvert/deepCopy/deepCopy.cpp
+++ src/examples/DataConvert/deepCopy/deepCopy.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <string>
 #include <iomanip>
 #include <ascent.hpp>
 #include <conduit.hpp>
@@ -11,17 +12,16 @@
 using namespace std;
 
 int nodeNum, element;
-string title, zoneType;
+string title, zone, zoneType;
 vector<string> variables;
-vector<vector<double>> value;
+vector<vector<double> > value;
 vector<int> vertexPos;
 
 int s2i(string str){
     int res = 0;
     stringstream ss;
     ss << str;
-    ss >> res;
-    ss.clear;
+    ss >> setprecision(16) >> res;
     return res;
 }
 
@@ -29,33 +29,50 @@ double s2d(string str){
     double res = 0;
     stringstream ss;
     ss << str;
-    ss >> res;
-    ss.clear;
+    ss >> setprecision(32) >> res;
     return res;
 }
 
-void ParseData(){
+string dataSplit(string str){
+    string res = "";
+    int indexBegin = -1;
+    int index = str.find("=");
+    while(index != -1){
+        res = res + string(str.begin() + (indexBegin + 1), str.begin() + index);
+        res = res + " " + "=" + " ";
+        indexBegin = index;
+        index = str.find("=", index + 1);
+    }
+    res = res + string(str.begin() + (indexBegin + 1), str.begin() + str.size());
+    return res;
+}
+
+int main(){
     ifstream in;
-    in.open("./flowfield.dat");
-    if(!in.is_open())
+    in.open("./flowfield.dat", ios::in); // /home/hoody/BUPT-ASCENT/src/examples/DataConvert/flowfield.dat
+    if(!in)
         cout << "file is not open!" << endl;
     string line;
-    int index = 0;
-    while(in >> line){
+    int count = 0;
+    while(getline(in, line)){
         stringstream ss;
         vector<string> vs;
-        string t;
-        line >> ss;
-        while(ss >> t){
-            if(t[t.size() - 1] == ',')
-                t = t.substr(0 ,t.size() - 1);
-            vs.push_back(t);
+        string temp;
+        line = dataSplit(line);
+        ss << line;
+        while(ss >> temp)
+        {
+            if(temp[temp.size() - 1] == ',')
+                temp = temp.substr(0 ,temp.size() - 1);
+            vs.push_back(temp);
+            // cout << "temp = " << temp << endl;
         }
         if(vs.size()){
             string upper = vs[0];
             transform(upper.begin(), upper.end(), upper.begin(), ::toupper);
             if(upper == "TITTLE"){
                 title = vs[2];
+                cout << title << endl;
             }
             else if(upper == "VARIABLES"){
                 variables.push_back(vs[2]);
@@ -63,52 +80,61 @@ void ParseData(){
                     getline(in, line);
                     line = string(line.begin() + 1, line.end() - 2);
                     variables.push_back(line);
+                    cout << variables[i] << endl;
                 }
             }
             else if(upper == "ZONE"){
                 zone = vs[3];
+                cout << zone << endl;
                 getline(in, line); // STRANDID=0, SOLUTIONTIME=0
             }
             else if(upper == "NODES"){
                 nodeNum = s2i(vs[2]);
+                 cout << nodeNum << endl;
                 element = s2i(vs[5]);
+                // cout << element << endl;
                 zoneType = vs[8];
+                // cout << zoneType << endl;
                 getline(in, line); // DATAPACKING=POINT
                 getline(in, line); // FACENEIGHBORCONNECTIONS=737908
                 getline(in, line); // FACENEIGHBORMODE=LOCALONETOONE
                 getline(in, line); // FEFACENEIGHBORSCOMPLETE=YES
                 getline(in, line); // DT=(SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE SINGLE )
+                count = 0;
             }
             else{
-                if(index < nodeNum){
+                if(count < nodeNum){
+                    value.resize(12);
                     for(int i = 0; i < 12; i++){
+                        // cout << s2d(vs[i]) << endl;
                         value[i].push_back(s2d(vs[i]));
+                        // if(count == 1){
+                        //     cout << s2d(vs[i]) << endl;
+                        // }
                     }
                 }
-                else if(index >= nodeNum && index < nodeNum + element){
+                else if(count >= nodeNum && count < nodeNum + element){
                     vertexPos.push_back(s2i(vs[0]));
                     vertexPos.push_back(s2i(vs[1]));
                     vertexPos.push_back(s2i(vs[2]));
                     vertexPos.push_back(s2i(vs[3]));
                 }
-                index++;
+                count++;
             }
         }
     }
     in.close();
-}
 
-int main(){
-    ParseData();
+
     conduit::Node n;
     n["coordsets/coords/type"] = "explicit";
-    n["coordsets/coords/values/x"] = Value[0];
-    n["coordsets/coords/values/x"] = Value[1];
-    n["coordsets/coords/values/x"] = Value[2];
-    n["coordsets/mesh/type"] = "unstructured";
-    n["coordsets/mesh/coordset"] = "coords";
-    n["coordsets/mesh/elements/shape"] = "tet";
-    n["coordsets/mesh/elements/connectivity"].set(vertexPos);
+    n["coordsets/coords/values/x"] = value[0];
+    n["coordsets/coords/values/y"] = value[1];
+    n["coordsets/coords/values/z"] = value[2];
+    n["topologies/mesh/type"] = "unstructured";
+    n["topologies/mesh/coordset"] = "coords";
+    n["topologies/mesh/elements/shape"] = "tet";
+    n["topologies/mesh/elements/connectivity"].set(vertexPos);
 
     for(int i = 3; i < 12; i++){
         stringstream ss;
@@ -130,6 +156,15 @@ int main(){
         n[res].set(value[i]);
     }
 
-    conduit::relay::io::save(n, "tet1.yaml");
+     conduit::relay::io::save(n, "tet1.yaml");
+     conduit::relay::io::load("tet1.yaml", n);
+    ascent::Ascent a;
+    a.open();
+    a.publish(n);
+
+    conduit::Node actions;
+    conduit::relay::io::load("test_tet_ascent.yaml", actions);
+    a.execute(actions);
+    a.close();
     return 0;
 }
